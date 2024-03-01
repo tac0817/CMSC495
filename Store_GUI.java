007c47d81fd2448200488b3de6b4a03d0b49db84
JRussell118 <157432078+JRussell118@users.noreply.github.com>
2024-03-01 13:28:05 -0500

    Modifications - JR

--- Store_GUI.java
+++ Store_GUI.java
@@ -42,7 +42,7 @@ public class Store_GUI extends JFrame {
                 }
             }
         });
-        
+
         resultList = new List();
         resultList.add("");
         resultList.addMouseListener(new MouseAdapter() {
@@ -51,7 +51,7 @@ public class Store_GUI extends JFrame {
                 cart.add(resultList.getSelectedItem() + "; " + storeLoc.getSelectedItem());
             }
         });
-        
+
         results = new JLabel();
         JButton searchButton = new JButton("Search");
         searchButton.addActionListener(new ActionListener() {
@@ -66,7 +66,7 @@ public class Store_GUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (cart.getItems() != null) {
-                    onCheckoutButtonClick();
+                    onToCheckoutButtonClick(currentUser);
                 } else {
                     JOptionPane.showMessageDialog(cButton, "Your cart is empty.", "Error", JOptionPane.ERROR_MESSAGE);
                 }
@@ -86,7 +86,6 @@ public class Store_GUI extends JFrame {
 
         JPanel userPanel = new JPanel();
         userPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 820, 10));
-        
 
         userPanel.add(welcome);
         userPanel.add(signoutButton);
@@ -159,12 +158,11 @@ public class Store_GUI extends JFrame {
 
     private void onSearchButtonClick() {
         String store = (String) storeLoc.getSelectedItem();
-        File file = new File("./src/inventory_system/Buy_Better_" + store + ".txt");
+        File file = new File("./src/inventorysystem/Buy_Better_" + store + ".txt");
 
         BufferedReader read;
         results.setText("");
         resultList.removeAll();
-        resultList.add("");
 
         if (searchBox.getText().contentEquals("Enter your product name here")
                 || searchBox.getText().isBlank()) {
@@ -195,71 +193,165 @@ public class Store_GUI extends JFrame {
         }
     }
 
-    private void onCheckoutButtonClick() {
+    private void onToCheckoutButtonClick(User u) {
         int itemNum = 0;
         float totalPrice = 0;
+        boolean rentals = false;
         int[] cartAmount = new int[cart.getItems().length];
-        String[] items = cart.getItems();
+
         for (int i = 0; i < cart.getItems().length; i++) {
             int num = 1;
-            for (int j = i + 1; j < items.length; j++) {
-                if (items[i].equals(items[j])) {
+            for (int j = i + 1; j < cart.getItems().length; j++) {
+                if (cart.getItems()[i].equals(cart.getItems()[j])) {
                     num++;
                     cartAmount[j] = -1;
                 }
             }
             if (cartAmount[i] != -1) {
                 cartAmount[i] = num;
+                itemNum++;
             }
         }
 
         JDialog checkoutPage = new JDialog(this, "Checkout", true);
-        checkoutPage.setSize(630, 500);
-        checkoutPage.setLayout(new FlowLayout(FlowLayout.CENTER, 550, 10));
+        checkoutPage.setSize(820, 500);
+        checkoutPage.setLayout(new FlowLayout(FlowLayout.CENTER, 820, 10));
         JPanel cartList = new JPanel();
-        
+
         JLabel checkoutMessage = new JLabel("Thank you for choosing to Buy Better!");
         JLabel itemLabel = new JLabel("Item Name:");
         JLabel numLabel = new JLabel("Item Amount:");
         JLabel priceLabel = new JLabel("Item Price:");
+        JLabel totalLabel = new JLabel();
         JLabel purchaseLabel = new JLabel("Choose a payment method:");
         JComboBox payment = new JComboBox(new String[]{"Credit Card (Visa)", "Credit Card (MasterCard)", "PayPal"});
         JButton checkout = new JButton("Checkout");
-        
+
         checkout.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < cartAmount.length; i++) {
+                    if (cartAmount[i] != -1) {
+                        checkoutItem(cart.getItems()[i], cartAmount[i]);
+                        rentItem(cart.getItems()[i], u);
+                    }
+                }
                 
+                JOptionPane.showMessageDialog(checkout, "Thank for shopping at Buy Better!");
+                checkoutPage.dispose();
             }
         });
-        
-        
+
         cartList.add(itemLabel);
         cartList.add(numLabel);
         cartList.add(priceLabel);
 
         for (int i = 0; i < cartAmount.length; i++) {
             if (cartAmount[i] != -1) {
-                cartList.add(new JLabel(items[i].split(":")[0]));
+                cartList.add(new JLabel(cart.getItems()[i].split(":")[0]));
                 cartList.add(new JLabel("" + cartAmount[i]));
-                cartList.add(new JLabel("" + getItemPrice(items[i])));
-                itemNum++;
+                cartList.add(new JLabel("" + getItemPrice(cart.getItems()[i]) * cartAmount[i]));
+                totalPrice += getItemPrice(cart.getItems()[i]) * cartAmount[i];
             }
         }
+
+        totalLabel.setText("Your total is: " + totalPrice);
         checkoutPage.add(checkoutMessage);
-        cartList.setLayout(new GridLayout(itemNum+2, 3, 15, 15));
+        cartList.setLayout(new GridLayout(itemNum + 2, 3, 50, 15));
         checkoutPage.add(cartList);
         checkoutPage.add(purchaseLabel);
+        checkoutPage.add(totalLabel);
         checkoutPage.add(payment);
         checkoutPage.add(checkout);
         checkoutPage.setVisible(true);
         checkoutPage.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 
+        cart.removeAll();
+        resultList.removeAll();
+        searchBox.setText("Enter your product name here");
+    }
+
+    private float getItemPrice(String i) {
+        File file = new File("./src/inventorysystem/Buy_Better_" + i.split("; ")[1] + ".txt");
+        BufferedReader read;
+
+        try {
+            read = new BufferedReader(new FileReader(file));
+            String fileLine = read.readLine();
+            while (fileLine != null) {
+                if (fileLine.split(";")[1].equals(i.split(":")[0])) {
+                    String price = fileLine.split(";")[4].replace("$", "");
+                    price = price.replace(",", "");
+                    return Float.parseFloat(price);
+                }
+                fileLine = read.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(this, "The item, " + i.split(":")[0] + "'s stock file could not be found.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this, "The item, " + i.split(":")[0] + "'s stock file is empty.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (NullPointerException ex) {
+            JOptionPane.showMessageDialog(this, "The item, " + i.split(":")[0] + "'s stock file is not entered.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return 0;
+    }
+
+    private void checkoutItem(String i, int n) {
+        File file = new File("./src/inventorysystem/Buy_Better_" + i.split("; ")[1] + ".txt");
+        BufferedReader read;
+        ArrayList<String> fileUpdate = new ArrayList<>();
+
+        try {
+            read = new BufferedReader(new FileReader(file));
+            String fileLine = read.readLine();
+            while (fileLine != null) {
+                if (fileLine.split(";")[1].equals(i.split(":")[0])) {
+                    int newStock = Integer.parseInt(fileLine.split(";")[5]) - n;
+                    fileUpdate.add(fileLine.replace(fileLine.split(";")[5], "" + newStock));
+                } else {
+                    fileUpdate.add(fileLine);
+                }
+                fileLine = read.readLine();
+            }
+            
+            
+            try (FileWriter writer = new FileWriter("./src/inventorysystem/Buy_Better_" + i.split("; ")[1] + ".txt")) {
+                for (String str : fileUpdate) {
+                    writer.write(str + System.lineSeparator());
+                }
+            }
+
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(this, "The item, " + i.split(":")[0] + "'s stock file could not be found.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this, "The item, " + i.split(":")[0] + "'s stock file is empty.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (NullPointerException ex) {
+            JOptionPane.showMessageDialog(this, "The item, " + i.split(":")[0] + "'s stock file is not entered.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
     
-    private float getItemPrice(String i){
-        float price = 0;
-        return price;
+    private void rentItem(String i, User u){
+        File file = new File("./src/inventorysystem/Buy_Better_" + i.split("; ")[1] + ".txt");
+        BufferedReader read;
+        try {
+            read = new BufferedReader(new FileReader(file));
+            try (FileWriter writer = new FileWriter("./src/inventorysystem/User_Rentals.txt")) {
+                String fileLine = read.readLine();
+                while (fileLine != null) {
+                    if(fileLine.split(";")[1].equals(i.split(":")[0]) && fileLine.split(";")[6].equals("rent")){
+                        String s = i.split("; ")[1] + ";" + u.getUsername() + ";" + i.split(":")[0] + ";" + fileLine.split(";")[5];
+                        writer.write(s + System.lineSeparator() );
+                    }
+                    fileLine = read.readLine();
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(this, "The item, " + i.split(":")[0] + "'s stock file could not be found.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this, "The item, " + i.split(":")[0] + "'s stock file is empty.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (NullPointerException ex) {
+            JOptionPane.showMessageDialog(this, "The item, " + i.split(":")[0] + "'s stock file is not entered.", "Error", JOptionPane.ERROR_MESSAGE);
+        } 
     }
 
     public static void main(String[] args) {
